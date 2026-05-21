00de8d2838513dec33bf8f129f3fbb8a08c5edc9
nadone1910 <nadonesamuel55@gmail.com>
2026-05-21 09:44:53 -0300

    Update stilo.py

--- stilo.py
+++ stilo.py
@@ -8,23 +8,15 @@ class Estilo:
     
     COR_BOTAO_CADASTRO = "#FF5E00"
     COR_BOTAO_LISTAR = "#27ae60"
-    COR_BOTAO_ATUALIZAR = "#f39c12"
-    COR_BOTAO_EXCLUIR = "#c0392b"
-    COR_BOTAO_RELATORIO = ["#e67e22", "#2980b9", "#8e44ad", "#16a085", "#d35400"]
     
     
+    BOTAO5 = {"bg": "#FF8800", "fg": "#ffffff", "font": ("Consolas", 10, "bold"), "width": 20, "relief": "flat", "bd": 2}
+    BOTAO4 = {"bg": "#00D126", "fg": "#ffffff", "font": ("Consolas", 10, "bold"), "width": 20, "relief": "flat", "bd": 2}
     BOTAO = {"bg": "#00A4D1", "fg": "#ffffff", "font": ("Consolas", 10, "bold"), "width": 20, "relief": "flat", "bd": 2}
     BOTAO_EXCLUIR= {"bg": "#FF0000", "fg": "#ffffff", "font": ("Consolas", 10, "bold"), "width": 20, "relief": "flat", "bd": 2}
-    BOTAO2 = {"bg": "#FF5E00", "fg": "#ffffff", "font": ("Consolas", 8, "bold"), "width": 20, "relief": "raised", "bd": 2}
-    BOTAO3 = {"bg": "#0074AA", "fg": "#ffffff", "font": ("Consolas", 8, "bold"), "width": 20, "relief": "raised", "bd": 2}
-
-    # ===== Fontes =====
+    
     FONTE_LABEL = ("Consolas", 10, "bold")
     FONTE_BOTAO = ("Consolas", 10, "bold")
-    FONTE_TEXT = ("Consolas", 10, "bold")
-    FONTE_TITULO = ("Consolas", 10, "bold")
 
-    # ===== Tamanhos =====
-    LARGURA_ENTRY = 80
+    LARGURA_ENTRY = 110
     LARGURA_BOTAO = 20
-
